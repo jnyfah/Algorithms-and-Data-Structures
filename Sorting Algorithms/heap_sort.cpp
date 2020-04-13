9ed5d6fd90f21580856acb1d68123a7c5efc0925
Jennifer Chukwu <jnyfaah@gmail.com>
2020-04-13 13:07:53 +0100

    O(nlogn)

---
+++ Sorting Algorithms/heap_sort.cpp
@@ -0,0 +1,28 @@
+void heapify(int arr[], int n, int i) {
+   int temp;
+   int largest = i;
+   int l = 2 * i + 1;
+   int r = 2 * i + 2;
+   if (l < n && arr[l] > arr[largest])
+      largest = l;
+   if (r < n && arr[r] > arr[largest])
+      largest = r;
+   if (largest != i) {
+      temp = arr[i];
+      arr[i] = arr[largest];
+      arr[largest] = temp;
+      heapify(arr, n, largest);
+   }
+}
+void heapSort(int arr[], int n) {
+   int temp;
+   for (int i = n / 2 - 1; i >= 0; i--)
+      heapify(arr, n, i);
+   for (int i = n - 1; i >= 0; i--) {
+      temp = arr[0];
+      arr[0] = arr[i];
+      arr[i] = temp;
+      heapify(arr, i, 0);
+   }
+}
+
