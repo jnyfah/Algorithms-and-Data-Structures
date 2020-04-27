48c756f5a0b6ddfa79a813c4c8194496f4e64cd7
Jennifer Chukwu <jnyfaah@gmail.com>
2020-04-27 20:53:54 +0100

    Create Recursion.cpp

---
+++ Recursion Algorithm/Recursion.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+//function declaration
+int fact(int n);
+int nonRecFact(int n);
+
+int main(){
+  //variable declaration
+  int n;
+
+  //input
+  std::cout<<"Enter n:  "<<std::endl;
+  std::cin>>n;
+
+  //recursive fact
+
+  std::cout<<"Recursion: "<< fact(n)<<std::endl;
+  //non-recursive fact
+
+    return 0;
+  }
+
+  //function definition
+  int fact(int n){
+    if(n == 0 || n == 1)
+      return 1;
+    else
+      return n * fact(n-1);
+  }
+
